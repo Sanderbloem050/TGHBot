d4eebdfcc1623faaa79d61ccc7326a2301e14fd6
Sander Bloem <sanderbloem87@gmail.com>
2022-08-12 22:06:49 +0200

    edited handleCommands

--- src/Functions/handlers/handleCommands.js
+++ src/Functions/handlers/handleCommands.js
@@ -14,7 +14,7 @@ module.exports = (client) => {
             const{commands, commandArray} = client;
             for(const file of commandFiles){
                 const command = require(`../../Commands/${folder}/${file}`);
-                client.commands.set(command.data.name, command);
+                commands.set(command.data.name, command);
                 commandArray.push(command.data.toJSON());
                 //console.log(`Command: ${command.data.name} has passed to the handler `)
             }
